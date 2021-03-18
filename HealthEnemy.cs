52e1dbea680b69e048786500880427eafff25c7d
troler36 <39880578+troler36@users.noreply.github.com>
2021-03-18 19:39:30 +0200

    Scripts

---
+++ HealthEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthEnemy : MonoBehaviour
+{
+    public float Health = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Dying
+        if(Health <= 0)
+        {
+            OnDeath();
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+    }
+
+    void OnDeath()
+    { 
+         Destroy(this.gameObject);        
+    }
+}
